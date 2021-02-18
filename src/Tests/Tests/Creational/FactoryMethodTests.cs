11c1e8299bfb8abdeae084ecb674a106f56b3464
amirhossein ghorbani <amirhosseinghorbani@hotmail.com>
2021-02-18 19:31:03 +0330

    Add factory method tests

---
+++ src/Tests/Tests/Creational/FactoryMethodTests.cs
@@ -0,0 +1,19 @@
+﻿using Creational.FactoryMathod;
+using Xunit;
+
+namespace Tests.Creational
+{
+    public class FactoryMethodTests
+    {
+
+        [Fact]
+        public void CreateProduct_Creator2_MustCreateProduct2()
+        {
+            var creator2 = new ConcreteCreator2();
+            IProduct product = creator2.Create();
+
+            Assert.True(product is ConcreteProduct2);
+        }
+
+    }
+}
